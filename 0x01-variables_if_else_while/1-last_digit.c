d0f225691256e94dc7bcc671c6c5be8137d5184d
Jude Amahiri <Amahirijude@gmail.com>
2022-11-25 16:21:53 +0000

    internalization

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	/* your code goes there */
 	/*try to run efficient code here pls*/
 
-	printf("Last digit of %d is %d and is", n, (n % 10))
+	printf("Last digit of %d is %d and is ", n, (n % 10));
 
 	if ((n % 10) > 5)
 	{
