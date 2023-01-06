54ec82668e167649e04d27bd9258ee1e5107d432
Jude Amahiri <Amahirijude@gmail.com>
2023-01-06 12:19:36 +0000

    True Learning

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 
 void print_to_98(int n)
 {
-	while (n < 98)
+	while (n <= 98)
 	{
 		printf("%d, ", n);
 		n++;
@@ -19,9 +19,6 @@ void print_to_98(int n)
 		printf("%d, ", n);
 		n--;
 	}
-
-	if (n == 98)
-		printf("%d", n);
 
 	printf("\n");
 }
