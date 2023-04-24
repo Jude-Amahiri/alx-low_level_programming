581e56d0438e936ac151615ac6c96e963de08c76
Jude Amahiri <Amahirijude@gmail.com>
2023-04-24 21:45:09 +0000

    Accelerated Learning

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,12 +4,10 @@
  * mul - multiplies two integers
  * @a: first integer
  * @b: second integer
- *
  * Return: the result of the multiplication
  */
 int mul(int a, int b)
 {
-        int result = a * b;
-
-        return (result);
+	int result = a * b;
+	return (result);
 }
