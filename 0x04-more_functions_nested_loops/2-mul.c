22bed762f006ccebb020b59a662a3897eadcc4eb
Jude Amahiri <Amahirijude@gmail.com>
2023-04-24 21:43:17 +0000

    Accelerated Learning

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * mul - checks for a digit 0 through 9
- * @a: digit to be multipled with
- * @b: digit to be multiplied
- * Return: 1 if true, 0 if false
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of the multiplication
  */
-
-int mul(int a, int b);
+int mul(int a, int b)
 {
-	int rslt = a * b;
+        int result = a * b;
 
-	return (rslt);
+        return (result);
 }
