cb1aa2a3f47b2cbf75355491e1f0977ee0693da2
Jude Amahiri <Amahirijude@gmail.com>
2023-04-24 21:13:07 +0000

    Accelerated Learning

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Jude Amahiri
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
