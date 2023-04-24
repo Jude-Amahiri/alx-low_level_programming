1a461bfce6b4b685da077901ada57eeb6767136f
Jude Amahiri <Amahirijude@gmail.com>
2023-04-24 21:38:09 +0000

    Accelerated Learning

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int mul(int a, int b);
+{
+	int rslt = a * b;
+
+	return (rslt);
+}
