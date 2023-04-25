d4eb3cb04fba8625c42320afa04800dc504880a3
Jude Amahiri <Amahirijude@gmail.com>
2023-04-25 03:39:20 +0000

    Accelerated Learning

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int row, digit;
+
+	for (row = '0'; row <= 10; row++)
+	{
+		for (digit = '0'; digit <= '14'; digit++)
+		{
+			if (digit > 0)
+			_putchar((digit / 10) + '0');
+		}
+		_putchar((i % 10) + '0');
+	}
+	
+	_putchar('\n');
+}
