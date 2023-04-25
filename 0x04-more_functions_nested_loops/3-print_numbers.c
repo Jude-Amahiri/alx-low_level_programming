acce7ef3e3606e54b6891fa012c24770308a35d5
Jude Amahiri <Amahirijude@gmail.com>
2023-04-25 02:54:55 +0000

    Accelerated Learning

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * File: 0-isupper.c
+ * Auth: Jude Amahiri
+ */
+
+/**
+ * print_numbers(void) - prints the numbers, from 0 to 9.
+ */
+
+void print_numbers(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		_putchar(n);
+		n++;
+	}
