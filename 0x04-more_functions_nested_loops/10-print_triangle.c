16799a0848758b34ae5aef557649e4eadb7c2798
Jude Amahiri <Amahirijude@gmail.com>
2023-06-27 09:02:08 +0000

    Random Fixes

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * print_triangle - use the character # to print the triangle
+  * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int tri;
+		int space;
+		int hash;
+
+		for (tri = 0; tri <= size; tri++)
+		{
+			for (space = 1; space <= (size - tri); space++)
+			{
+				_putchar(' ');
+			}
+
+			for (hash = 0; hash <= tri; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
