e67bad7a4038ba830bc61a934163479095b259b1
Jude Amahiri <amahirijude@gmail.com>
2023-10-06 14:51:28 +0000

    One Byte

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,70 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+
+	int len = 0, i;
+	unsigned int sum = 0;
 
 	if (b == NULL)
-		return (0);
+		return (sum);
 
-	len = _strlen(b);
-	while (len--)
+	/* find string length */
+	while (b[len] != '\0')
+		len++;
+	len -= 1;
+
+	/* iterate string and if '1' then multiply by power of 2 */
+	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
+	i = 0;
+	while (b[i])
 	{
-		if (b[len] != 48 && b[len] != 49)
-			return (0);
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (sum);
 
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
+		len--;
 	}
 
 	return (sum);
 }
 
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
-}
+/*
+ * alternative method not using bitwise but a power of 2
+ *
+ *
+ * unsigned int binary_to_uint(const char *b)
+ *{
+ *
+ *	int len = 0, pow = 1;
+ *	unsigned int sum = 0, error = 0;
+ *
+ *	if (b == NULL)
+ *		return (error);
+ *
+ *	while (b[len] != '\0') // find string length
+ *		len++;
+ *	len -= 1;
+ *
+ *	while (len >= 0) // iterate from back of string
+ *	{
+ *		if ((b[len] != '0') && (b[len] != '1'))
+ *			return (error);
+ *
+ *		if (b[len] == '1') // add appropriate power of 2 if '1'
+ *			sum += pow;
+ *
+ *		pow *= 2; // update power of 2
+ *		len--;
+ *	}
+ *
+ *	return (sum);
+ *}
+ */
