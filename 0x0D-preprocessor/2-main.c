c3af34aa33fde5aea728d0873e9167a075cfd62a
Jude Amahiri <amahirijude@gmail.com>
2024-03-26 15:58:00 +0100

    ws

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - prints current filename to std output
- * Return: 0 on success
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
