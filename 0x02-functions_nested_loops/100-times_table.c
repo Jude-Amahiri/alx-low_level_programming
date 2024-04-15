992764d16effd52743a279d3077d1093480327ca
Jude-Amahiri <113861126+Jude-Amahiri@users.noreply.github.com>
2024-04-15 20:56:09 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,17 +7,17 @@
 
 void print_times_table(int n)
 {
-	int row;
 	int column;
+	int row;
 	int product;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (column = 0; column <= n; column++)
 		{
-			for (column = 0; column <= n; column++)
+			for (row = 0; row <= n; row++)
 			{
-				product = (row * column);
+				product = (column * row);
 				if (column == 0)
 					_putchar('0' + product);
 				else
