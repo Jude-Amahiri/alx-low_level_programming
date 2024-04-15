ac7fdeb72660eb44f223ab28f4bf9d60c997f41e
Jude Amahiri <amahirijude@gmail.com>
2024-04-15 21:18:22 +0100

    Betty was Appeased

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
+/**
+ * main - print sum of even Fibonacci sequence up to 4,000,000
+ * Return: 0
+ */
+
 int main(void)
 {
-    int sumOfEvenFibonacci = 0;
-    int previousNumber = 1;
-    int currentNumber = 1;
-    int nextNumber;
+	int sumOfEvenFibonacci = 0;
+	int previousNumber = 1;
+	int currentNumber = 1;
+	int nextNumber;
 
-    while (currentNumber <= 4000000)
-    {
-        nextNumber = previousNumber + currentNumber;
-        previousNumber = currentNumber;
-        currentNumber = nextNumber;
+	while (currentNumber <= 4000000)
+	{
+		nextNumber = previousNumber + currentNumber;
+		previousNumber = currentNumber;
+		currentNumber = nextNumber;
 
-        if (currentNumber % 2 == 0)
-        {
-            sumOfEvenFibonacci += currentNumber;
-        }
-    }
+		if (currentNumber % 2 == 0)
+		{
+			sumOfEvenFibonacci += currentNumber;
+		}
+	}
+	printf("Sum of even Fibonacci numbers: %d\n", sumOfEvenFibonacci);
 
-    printf("Sum of even Fibonacci numbers: %d\n", sumOfEvenFibonacci);
-
-    return 0;
+	return (0);
 }
-
